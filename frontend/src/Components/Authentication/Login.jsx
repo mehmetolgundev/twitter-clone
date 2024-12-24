da289b000fc85dd945dd8f8ca6aedb13753c0a28
mehmet olgun <mehmetolgun@mehmet-MacBook-Pro.local>
2024-12-24 21:45:27 +0300

    AuthProvider,Login components developed

--- frontend/src/Components/Authentication/Login.jsx
+++ frontend/src/Components/Authentication/Login.jsx
@@ -11,7 +11,7 @@ export default function Login(){
                 </div>
                 <div className="col-md-6 justify-content-center align-items-center">
              <h1 className="text-white fw-bold title-font" >Happening now</h1>
-                <h2 className="text-white display-6">Join today.</h2>
+                <h2 className="text-white display-6">Join today</h2>
              <button className="btn btn-primary">Create account</button>
              <button className="btn btn-secondary">Sign in</button>
                       </div>
