df8bd60ca007349a517ec9522dbd20d0be117be6
mehmet olgun <mehmetolgun@mehmet-MacBook-Pro.local>
2025-01-04 16:59:46 +0300

    Login page recoded with tailwindcss

--- frontend/src/Components/Welcome.jsx
+++ frontend/src/Components/Welcome.jsx
@@ -9,14 +9,15 @@ export default function Welcome() {
       <BsTwitterX className="text-white w-1/6 h-1/6 md:w-1/2 md:h-1/2" />
       </div>
       <div className="order-2 w-full p-4 md:w-1/2 flex-1 h-full flex flex-col justify-start items-start md:justify-center md:items-start">
-       <h1 className="self-start text-white text-6xl font-bold mb-24">Happening now</h1>
+       <h1 className="self-start text-white text-7xl font-bold mb-24">Happening now</h1>
        <h2 className="text-white text-2xl font-bold mb-4">Join today.</h2>
-       <button className="bg-twitter-blue hover:bg-twitter-blue-hover text-white font-bold px-4 py-2 rounded-full w-3/4 md:w-1/2 mb-12">Create account</button>
-       <p className="text-white text-md font-bold mb-2">Already have an account?</p>
+       <button className="bg-twitter-blue hover:bg-twitter-blue-hover text-white font-bold px-4 py-2 rounded-full w-3/4 md:w-1/2 mb-4">Create account</button>
+       <hr className="w-3/4 md:w-1/2 text-white mb-8"></hr>
+       <p className="text-white text-md font-bold mb-4">Already have an account?</p>
        <button className="bg-black text-twitter-blue hover:bg-twitter-signin-hover font-bold border border-twitter-border px-4 py-2 rounded-full w-3/4 md:w-1/2">Sign in</button>
       </div>
       </div>
-      <div className="w-full flex h-fit flex-wrap justify-center items-center text-sm gap-x-4 mb-4">
+      <div className="w-full flex h-fit flex-wrap justify-center items-center text-sm gap-x-4 mb-4 text-twitter-border">
       <p>About</p>
       <p>Download the X app</p>
       <p>Help Center</p>
